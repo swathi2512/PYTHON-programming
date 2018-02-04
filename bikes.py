d16da755bdd9fa0f7ddb36fb4dad72eb41df2d65
swathi2512 <36121399+swathi2512@users.noreply.github.com>
2018-02-04 15:18:04 +0530

    Add files via upload

---
+++ bikes.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
+bikes = ['trek','redline','giant']
+first_bike = bikes[0]
+second_bike = bikes[1]
+last_bike = bikes[2]
+bikes.append('ktm')
+bikes.append('ducati')
+print(bikes)
+
+
+
+
